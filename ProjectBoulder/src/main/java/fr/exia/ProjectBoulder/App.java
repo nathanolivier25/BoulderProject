e64842cfb55a2a766d98c5cf5a256beb4404b01b
Naiiden <hugo.plissonneau@viacesi.fr>
2017-06-14 08:51:40 +0200

    Test commit

---
+++ ProjectBoulder/src/main/java/fr/exia/ProjectBoulder/App.java
@@ -0,0 +1,13 @@
+package fr.exia.ProjectBoulder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
