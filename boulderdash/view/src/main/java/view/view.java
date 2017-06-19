447fd03baa024be3d85c57427e673ac570e9bf91
CorentinA1 <corentin.barataud@viacesi.fr>
2017-06-19 09:32:14 +0200

    view

---
+++ boulderdash/view/src/main/java/view/view.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class view 
+{
+	private ArrayList <ElementSprite> image;
+	private JFrame windows;
+	private OrderType lastOrder;
+	
+	public view()
+	{
+		
+	}
+	
+	public void DrawMap(ArrayList<Entity>)
+	{
+		
+	}
+	
+	public void loadImage()
+	{
+		
+	}
+	
+	public void keyPressed(e KeyEvent)
+	{
+		
+	}
+	
+	public void keyReleased(e KeyEvent)
+	{
+		
+	}
+	
+	public OrderType GetOrder();
+	{
+		
+	}
+}
+
+	
+
