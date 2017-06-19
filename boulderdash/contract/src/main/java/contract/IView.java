421464eba4e273156e0895859acebda1455ec818
Naiiden <hugo.plissonneau@viacesi.fr>
2017-06-19 16:06:31 +0200

    IView

--- boulderdash/contract/src/main/java/contract/IView.java
+++ boulderdash/contract/src/main/java/contract/IView.java
@@ -2,7 +2,7 @@ package contract;
 import java.util.ArrayList;
 public interface IView {
 
-	public void DrawMap(ArrayList <IEntity> );
+	public void DrawMap(ArrayList <IEntity> View);
 	public OrderType GetOrder();
 	
 }
