402a24851be198b3476d81c9fcff587e54952cf7
Naiiden <hugo.plissonneau@viacesi.fr>
2017-06-20 11:15:10 +0200

    Add LevelDAO

---
+++ boulderdash/model/src/main/java/model/dao/LevelDAO.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.Model;
+
+public class LevelDAO {
+	protected Model model;
+
+	public Model getModel() {
+		return model;
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
