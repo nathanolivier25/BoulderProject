e9c25994d7b2436238464890adcd5c8c906f06a9
Nathan <Nathan@Pc-nathan>
2017-06-21 16:39:32 +0200

    view

--- boulderdash/view/src/main/java/view/View.java
+++ boulderdash/view/src/main/java/view/View.java
@@ -33,7 +33,7 @@ public class View implements IView, KeyListener{
 		windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		windows.setVisible(true);
 		
-		mapSizeX = 20;
+		mapSizeX = 10;
 		mapSizeY = 20;
 		
 		
