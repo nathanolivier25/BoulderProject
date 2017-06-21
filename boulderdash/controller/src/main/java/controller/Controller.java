bb2efb95ad60a00e0ec158436f3c419591b71b3a
Naiiden <hugo.plissonneau@viacesi.fr>
2017-06-21 15:47:09 +0200

    timer added

--- boulderdash/controller/src/main/java/controller/Controller.java
+++ boulderdash/controller/src/main/java/controller/Controller.java
@@ -38,6 +38,12 @@ public class Controller
 			if(model.isLost() == true || model.isVictory() == true)
 				running = false;
 					
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
