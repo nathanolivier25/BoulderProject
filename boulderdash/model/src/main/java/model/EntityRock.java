421d8763d3327c6b8469e53a9c8d7e9e5fae8de2
Nathan <Nathan@Pc-nathan>
2017-06-22 15:49:29 +0200

    rock

--- boulderdash/model/src/main/java/model/EntityRock.java
+++ boulderdash/model/src/main/java/model/EntityRock.java
@@ -6,84 +6,39 @@ import contract.EntityType;
 import contract.IEntity;
 import contract.OrderType;
 
-/**
- * EntityRock is the class that define the "behavior" of our rocks.
- * Since it's an Entity, it extends the Entity class.
- * 
- * It's defined by the following elements :
- * <ul>
- * <li>
- * @author Group 1
- * @version 1
- */
-
 public class EntityRock extends Entity{
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param Posx The position in x of the rock.
-	 * 
-	 * @param Poxy The position in y of the rock.
-	 */
 	public EntityRock(int Posx, int Posy) {
 		super(Posx, Posy, EntityType.ROCK);
 	}
 
-	/**
-	 * 
-	 */
+	
 	public ArrayList<IEntity> Update(ArrayList<IEntity> test, OrderType order) {
 
-		/**
-		 * 
-		 */
+		boolean canFall = true;
 		for (int i = 0; i < test.size(); i++)
 		{
 			
 			if (test.get(i).GetPosx() == this.getPosx() && test.get(i).GetPosy() == this.getPosy()+1)
 			{
-				//If there is another rock bellow the rock, it will not move.
-				if(test.get(i).getType() == EntityType.ROCK)
+				//If there is something else than an empty place downside the rock won't fall
+				if((test.get(i).getType() != EntityType.VOID) || (test.get(i).getType() !=EntityType.PLAYER))
 				{
-					//If there is another rock bellow the rock AND if there is a void on the right of the first rock AND on the right of the second rock,
-					//The first rock will fall on the right of the other rock.
-					if(test.get(i).GetPosx() == this.getPosx()+1 && test.get(i).GetPosy() == this.getPosy() && test.get(i).getType() == EntityType.VOID && test.get(i).GetPosx() == this.getPosx()+1 && test.get(i).GetPosy() == this.getPosy()+1 && test.get(i).getType() == EntityType.VOID)
-					{
-						Posx = Posx+1;
-						Posy = Posy+1;
-					}
-					
-					//If there is another rock bellow the rock AND if there is a void on the left of the first rock AND on the left of the second rock,
-					//The first rock will fall on the left of the other rock.
-					if (test.get(i).GetPosx() == this.getPosx()-1 && test.get(i).GetPosy() == this.getPosy() && test.get(i).getType() == EntityType.VOID && test.get(i).GetPosx() == this.getPosx()-1 && test.get(i).GetPosy() == this.getPosy()+1 && test.get(i).getType() == EntityType.VOID)
-					{
-						Posx = Posx-1;
-						Posy = Posy-1;
-					}
+					canFall = false;
 				}
 				
-				//If there is a "void" bellow the rock, it will fall at the position of the void.
-				else if(test.get(i).getType() == EntityType.VOID)
-				{
-					Posy = Posy+1;
-				}
-				
-				//If there is an enemy bellow the rock, it will fall on it.
-				else if(test.get(i).getType() == EntityType.ENEMY)
-				{
-					Posy = Posy+1;
-				}
-				
-				//If there is a player bellow the rock, it will fall on it.
-				else if(test.get(i).getType() == EntityType.PLAYER)
-				{
-					Posy = Posy+1;
-				}
 				
 			}
 
 		} 	
+		
+		
+		if(canFall)
+		{
+			Posy = Posy + 1;
+		}
+		
+		
 		return test;
 	
 		}
