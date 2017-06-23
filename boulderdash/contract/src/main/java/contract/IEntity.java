8c3fd432f3282a38645b67df01683ba5243f8cf3
Naiiden <hugo.plissonneau@viacesi.fr>
2017-06-23 12:50:10 +0200

    Javadoc Contract

--- boulderdash/contract/src/main/java/contract/IEntity.java
+++ boulderdash/contract/src/main/java/contract/IEntity.java
@@ -15,6 +15,9 @@ public interface IEntity
 	public EntityType typeID = null;
 	
 	
+	/**
+	 * There are the Getters and Setters from Posx, Posy and typeID.
+	 */
 	public int GetPosx();
 	
 	public void setPosx(int posx);
@@ -23,8 +26,13 @@ public interface IEntity
 	
 	public void setPosy(int posy);
 
+
 	public EntityType getType();
 	
+	/**
+	 * @param test That's the name of the ArrayList.
+	 * @param order That's an OrderType.
+	 */
 	public ArrayList<IEntity> Update(ArrayList<IEntity> test, OrderType order);
 
 	
