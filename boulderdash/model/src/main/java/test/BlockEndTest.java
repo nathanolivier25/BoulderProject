b248197fd11c60e1d194e406d7bf8cb200915a52
CorentinA1 <corentin.barataud@viacesi.fr>
2017-06-23 15:16:56 +0200

    test block end

--- boulderdash/model/src/main/java/test/BlockEndTest.java
+++ boulderdash/model/src/main/java/test/BlockEndTest.java
@@ -8,6 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import contract.EntityType;
+import model.BlocDirt;
+import model.BlockEnd;
+
 public class BlockEndTest {
 
 	@BeforeClass
@@ -27,8 +31,24 @@ public class BlockEndTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetType() {
+		EntityType typeTest = EntityType.END;
+		final BlockEnd e = new BlockEnd(0, 0);
+		assertEquals(e.getType(), typeTest);
+	}
+
+	@Test
+	public void testGetPosx() {
+		final int posxTest1 = 20;
+		final BlockEnd e = new BlockEnd(posxTest1, 0);
+		assertEquals (e.GetPosx(), posxTest1);
+	}
+
+	@Test
+	public void testGetPosy() {
+		final int posyTest2 = 5;
+		final BlockEnd e = new BlockEnd(0, posyTest2);
+		assertEquals (e.GetPosy(), posyTest2);
 	}
 
 }
