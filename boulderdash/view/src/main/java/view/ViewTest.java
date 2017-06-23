182b41da571365296415e8a573ad08598a09e3f6
CorentinA1 <corentin.barataud@viacesi.fr>
2017-06-23 10:46:48 +0200

    test

---
+++ boulderdash/view/src/main/java/view/ViewTest.java
@@ -0,0 +1,49 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLoadImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyPressed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyReleased() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrder() {
+		fail("Not yet implemented");
+	}
+
+}
